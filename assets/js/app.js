03acde7704169572e9383b2d872794890427f560
Enrique Marín <enriquemarin_sierra@hotmail.com>
2025-04-04 15:01:44 +0200

    add using hooks to dynamically add line numbers
    - `Pure JavaScript` is used to create the hook that manages automatic numbering in a text area attached to the text area where the gist code is written.
    - Another listener is also added to the textarea to handle the scroll event and force it to scroll in parallel with the other text area that manages line numbering.
    - A CSS class is added to improve the appearance of the textarea's scroll bar.

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,18 +18,53 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+
+let Hooks = {};
+
+Hooks.UpdateLineNumbers = {
+  mounted() {
+    this.el.addEventListener("input", () => {
+      this.updateLineNumbers()
+    })
+
+    this.el.addEventListener("scroll", () => {
+      const lineNumberText = document.querySelector("#line-numbers");
+
+      // This way, the two text areas will run in parallel
+      // when the text area in which we write scrolls automatically.
+      lineNumberText.scrollTop = this.el.scrollTop;
+    })
+
+    // We also call the update function when mounting our input.
+    this.updateLineNumbers()
+  },
+
+  updateLineNumbers() {
+    const lineNumberText = document.querySelector("#line-numbers");
+    if (!lineNumberText) return;
+
+    // `this` is the element to which the hook is attached,
+    // in this case, the text area attached to `#line-numbers`
+    // in which we put our code.
+    const lines = this.el.value.split("\n");
+    const numbers = lines.map((_, index) => index + 1).join("\n") + "\n";
+
+    lineNumberText.value = numbers;
+  }
+};
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken}
+  params: { _csrf_token: csrfToken },
+  hooks: Hooks,
 })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
