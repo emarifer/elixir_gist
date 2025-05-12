0ae260152227bdf42ab23f3c0f4dd7a00d8a9166
Enrique Marín <enriquemarin_sierra@hotmail.com>
2025-05-12 13:56:16 +0200

    fix bug when showing/hiding the password definitely

--- lib/elixir_gist_web/live/user_login_live.ex
+++ lib/elixir_gist_web/live/user_login_live.ex
@@ -40,7 +40,7 @@ defmodule ElixirGistWeb.UserLoginLive do
           <div
             phx-click={
               JS.toggle_class("hero-eye-slash", to: {:inner, "span"})
-              |> JS.toggle_attribute({"type", "text", "password"}, to: ":has(+ div) input")
+              |> JS.toggle_attribute({"type", "text", "password"}, to: ":has(+ div) div input")
               |> JS.toggle_attribute({"title", "Show password", "Hide password"})
             }
             title="Show password"
